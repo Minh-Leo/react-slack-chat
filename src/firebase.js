f42fcfd57c64825f9a693f57b7b6d48c57539e50
Minh Bui <tranduyminh.bui@gmail.com>
2020-05-08 15:13:25 +1000

    added semantic ui css, finished Register component

---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
+import 'firebase/storage';
+
+var firebaseConfig = {
+  apiKey: 'AIzaSyB0SaDX3COZmzaVG2yDIJsOle2qnx6Q93s',
+  authDomain: 'slack-chat-app-765e8.firebaseapp.com',
+  databaseURL: 'https://slack-chat-app-765e8.firebaseio.com',
+  projectId: 'slack-chat-app-765e8',
+  storageBucket: 'slack-chat-app-765e8.appspot.com',
+  messagingSenderId: '73573684477',
+  appId: '1:73573684477:web:c1c2edd8d0310311005a94',
+  measurementId: 'G-HKYXHRLD0V',
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+// firebase.analytics();
+
+export default firebase;
