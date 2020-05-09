83be905a81b32385de11ea5c52eea09cbf241763
Minh Bui <tranduyminh.bui@gmail.com>
2020-05-09 17:34:28 +1000

    added Spinner component with Loader from semantic-ui

--- src/index.js
+++ src/index.js
@@ -1,26 +1,61 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route, withRouter } from 'react-router-dom';
+import * as serviceWorker from './serviceWorker';
+import firebase from './firebase';
+
 import App from './components/App';
 import Login from './components/Auth/Login';
 import Register from './components/Auth/Register';
-import * as serviceWorker from './serviceWorker';
+import Spinner from './components/Spinner';
+
+import { createStore } from 'redux';
+import { Provider, connect } from 'react-redux';
+import rootReducer from './reducers/index';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+import { setUser } from './actions/index';
 
 import 'semantic-ui-css/semantic.min.css';
 
-const Root = () => (
-  <BrowserRouter>
-    <Switch>
-      <Route exact path='/' component={App} />
-      <Route path='/login' component={Login} />
-      <Route path='/register' component={Register} />
-    </Switch>
-  </BrowserRouter>
+class Root extends React.Component {
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.props.setUser(user);
+        this.props.history.push('/');
+      }
+    });
+  }
+
+  render() {
+    return this.props.isLoading ? (
+      <Spinner />
+    ) : (
+      <Switch>
+        <Route exact path='/' component={App} />
+        <Route path='/login' component={Login} />
+        <Route path='/register' component={Register} />
+      </Switch>
+    );
+  }
+}
+
+const mapStateToProps = (state) => ({
+  isLoading: state.user.isLoading,
+});
+
+const RootWithAuth = withRouter(connect(mapStateToProps, { setUser })(Root));
+
+const store = createStore(rootReducer, composeWithDevTools());
+
+ReactDOM.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <RootWithAuth />
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById('root')
 );
 
-ReactDOM.render(<Root />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
