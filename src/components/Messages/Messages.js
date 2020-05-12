956410b6c55111fd77c41de53172084c4f1f21df
Minh Bui <tranduyminh.bui@gmail.com>
2020-05-12 21:27:05 +1000

    overall layout for message panel

--- src/components/Messages/Messages.js
+++ src/components/Messages/Messages.js
@@ -1,8 +1,22 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import { Segment, Comment } from 'semantic-ui-react';
+
+import MessagesHeader from './MessagesHeader';
+import MessageForm from './MessageForm';
 
 export class Messages extends Component {
   render() {
-    return <div>messages</div>;
+    return (
+      <Fragment>
+        <MessagesHeader />
+
+        <Segment>
+          <Comment.Group className='messages'></Comment.Group>
+        </Segment>
+
+        <MessageForm />
+      </Fragment>
+    );
   }
 }
 
