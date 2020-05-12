b2a30ff2d008defe03b7684580ab0529833f0e39
Minh Bui <tranduyminh.bui@gmail.com>
2020-05-12 22:40:19 +1000

    Message component finished, message can be added to database and display on panel

--- src/components/Messages/MessageForm.js
+++ src/components/Messages/MessageForm.js
@@ -1,17 +1,81 @@
 import React, { Component } from 'react';
+import firebase from '../../firebase';
 import { Segment, Input, Button } from 'semantic-ui-react';
 
 export class MessageForm extends Component {
+  state = {
+    message: '',
+    channel: this.props.currentChannel,
+    user: this.props.currentUser,
+    loading: false,
+    errors: [],
+  };
+
+  handleChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  createMessage = () => {
+    const message = {
+      timestamp: firebase.database.ServerValue.TIMESTAMP,
+      content: this.state.message,
+      user: {
+        id: this.state.user.uid,
+        name: this.state.user.displayName,
+        avatar: this.state.user.photoURL,
+      },
+    };
+    return message;
+  };
+
+  sendMessage = () => {
+    const { messagesRef } = this.props;
+    const { message, channel } = this.state;
+
+    if (message) {
+      this.setState({ loading: true });
+      messagesRef
+        .child(channel.id)
+        .push()
+        .set(this.createMessage())
+        .then(() => {
+          this.setState({
+            loading: false,
+            message: '',
+            errors: [],
+          });
+        })
+        .catch((err) => {
+          console.error(err);
+          this.setState({
+            errors: this.state.errors.concat(err),
+            loading: false,
+          });
+        });
+    } else {
+      this.setState({
+        errors: this.state.errors.concat({ message: 'Add a message' }),
+      });
+    }
+  };
+
   render() {
+    const { errors, message, loading } = this.state;
+
     return (
       <Segment className='message__form'>
         <Input
           fluid
           name='message'
+          value={message}
           style={{ marginBottom: '1em' }}
           label={<Button icon={'add'} />}
           labelPosition='left'
           placeholder='write your message'
+          onChange={this.handleChange}
+          className={
+            errors.some((err) => err.message.includes('message')) ? 'error' : ''
+          }
         />
         <Button.Group icon widths='2'>
           <Button
@@ -19,6 +83,8 @@ export class MessageForm extends Component {
             content='Add Reply'
             labelPosition='left'
             icon='edit'
+            onClick={this.sendMessage}
+            disabled={loading}
           />
           <Button
             color='teal'
